a3038a3e8f62d3d3500eeb5e2c00db4b25fd0a1f
assiffdev <assiffdev@gmail.com>
2025-03-07 11:06:42 +0500

    updated route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,6 +1,7 @@
 import OpenAI  from "openai";
 import { OpenAIStream , StreamingTextResponse} from "ai"
 import { DataAPIClient } from "@datastax/astra-db-ts";
+import { Models } from "openai/resources/models.mjs";
 
 const { ASTRA_DB_NAMESPACE , 
     ASTRA_DB_COLLECTION ,
@@ -19,9 +20,60 @@ const db = client.db(ASTRA_DB_API_ENDPOINT , {namespace: ASTRA_DB_NAMESPACE})
 export async function POST(req: Request){
     try{
         const { messages } = await req.json()
-        const lastMessage = messages[messages.length-1]?.content
-    }catch(err){
-        console.log("Error in querying db ....")
+        const latestMessage = messages[messages.length - 1]?.content;
+
+        let docContext = '';
+
+        const embedding = await openai.embeddings.create({
+            model:"text-embedding-3-small",
+            input: latestMessage,
+            encoding_format: "float"
+        });
         
+        try{
+            const collection = await db.collection(ASTRA_DB_COLLECTION);
+            const cursor = collection.find(null, {
+                sort:{
+                    $vector:embedding.data[0].embedding,
+                },
+                limit: 10,
+            })
+
+            const documents = await cursor.toArray();
+            const docsMap = documents?.map(doc => doc.text);
+            docContext = JSON.stringify(docsMap)
+        }catch(err){
+            console.log("Error in querying db ....")
+            docContext = "";
+        }
+        const template = {
+            role: "system",
+            content: `You are AI assistant who know everything about Formula One.
+            Use the below context to argument what you know about Formula One racing.
+            The context will provide you with most recent page data from wikipedia,
+            the official F1 website and others.
+            If the context does not include the information you need answer based on your existing
+            knowledge and do not mention the source of your information or what the context does or does not
+            include.
+            Format responses using markdown where applicable and do not return images.
+            -----------------------
+            START CONTEXT
+            ${docContext}
+            END CONTEXT
+            -----------------------
+            QUESTION:${latestMessage}
+            -----------------------`
+        }
+        const response = await openai.chat.completions.create(
+            {
+                model: 'chatgpt-4',
+                stream: true,
+                messages: [template, ...messages]
+            }
+        );
+        const stream = OpenAIStream(response);
+        return new StreamingTextResponse(stream);
+    }catch(err){
+        throw err;
     }
 }
